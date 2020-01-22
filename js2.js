55f286df939691df6b75c15541c99321ff5b8a52
Evgenii Mishkin <emishkin@mail.greenriver.edu>
2020-01-22 14:59:23 -0500

    JS3

--- js2.js
+++ js2.js
@@ -1,3 +1,4 @@
+/*
 for (var i = 1; i < 101; i++) {
     if ((i%3 === 0) && (i%5 === 0)) {
         console.log("Hee Haw!");
@@ -9,13 +10,22 @@ for (var i = 1; i < 101; i++) {
     }
 }
 
+*/
+
 function heeHaw() {
     var num = prompt("Enter any number");
     if (isNaN(num)) {
         console.log("Invalid input");
     } else {
         for (var i = 1; i <= num; i++) {
-            console.log(i);
+            if ((i%3 === 0) && (i%5 === 0)) {
+                console.log("Hee Haw!");
+            } else if (i%3 === 0) {
+                console.log("Hee!");
+            }
+            else {
+                console.log(i);
+            }
         }
     }
 }
